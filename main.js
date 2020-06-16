5d7f9a84553403e8dfee58b041af6ed8c77c4517
Chase Ainsworth <chase.ainsworth@codeimmersives.com>
2020-06-16 08:18:28 -0400

    All 19 passed

--- main.js
+++ main.js
@@ -2,7 +2,47 @@
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
 
+function isItLong(overTwenty) {
+  if (overTwenty.length > 20) {
+  return "That's a long string!" }
+}
 
+function isItMedium(between) {
+  if (between.length <= 20 && between.length >= 10) {
+  return "That's a regular sized string!"
+  }
+}
+
+function isItShort(tiny) {
+  if (tiny.length < 10) {
+  return "That's a small string!";
+} else {
+  return "That's not a small string!"
+  }
+}
+
+function howLongIsMyString(tellMe) {
+  if (tellMe.length > 20) {
+  return "That's a long string!";
+  } else if (tellMe.length <= 20 && tellMe.length >= 10) {
+  return "That's a regular sized string!";
+  } else { (tellMe.length < 10)
+  return "That's a small string!";
+  }
+}
+
+function instructorHeight(height) {
+  if (height === "Colin") {
+  return 62;
+  } else if (height === "Mesuara") {
+  return 69;
+  } else { 
+  return "I don't know that instructor!";
+  }
+}
+// function isItLong(twentyCharactersPlus) {
+//   return "That's a long string" if twentyCharactersPlus > 20
+// }
 
 
 
